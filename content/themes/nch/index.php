e17dfe39da1528f71c9b19a28197156eb5d6b7e5
Asif Ahmed <asif@stitchdesignco.com>
2017-04-03 17:42:36 -0400

    Add HTML skeleton to index.php

--- content/themes/nch/index.php
+++ content/themes/nch/index.php
@@ -14,5 +14,60 @@
 
   get_header(); ?>
 
-<h1>Live Reloaded please boy</h1>
+<!-- ==== Section: MAIN ==== -->
+<section id="main">
+</section>
+
+<!-- ==== Section: Land ==== -->
+<section id="land">
+	<h1>The Land</h1>
+	<h2>A short, 20 miles from south Charlotte.</h2>
+	<h3>North Corner Haven&trade; is located on over 600 acres of rolling hills, pastures and ponds for guests to explore. Founded in 1972 by The Dooley Family, the North Corner Haven team is transforming the land to foster sustainable farming and permaculture principles in daily practice.</h3>
+</section>
+
+<!-- ==== Section: Invited ==== -->
+<section id="invited">
+	<h1>You're Invited</h1>
+	<h2>Come and enjoy nature’s bounty in an idyllic farm-to-table setting.</h2>
+	<h3>We invite you to immerse yourself in the land as you celebrate, reflect, and embark on new experiences with your family, friends or colleagues. Whether you seek the silence and peace of yoga in a secluded forest setting, or the challenge of sporting clays and mountain biking on trails, guests will find opportunities to fill their souls and nourish their bodies.</h3>
+</section>
+
+<!-- ==== Section: newsletter ==== -->
+<section id="newsletter">
+	<h1>Newsletter Sign-up</h1>
+	<h2>Stay in the loop.</h2>
+	<h3>Offering farm tours and education as well as event venues and a range of outdoor activities, North Corner Haven offers an unforgettable, one of a kind guest experience. Stay tuned as we grow our business. We look forward to seeing you on the farm!</h3>
+	<form>
+	  <input type="name">
+	  <input type="email">
+	  <input type='submit'>
+	</form>
+</section>
+
+<!-- ==== Section: Insta ==== -->
+<section id="insta">
+	<h1>Follow Along On Instagram</h1>
+	<div id="feed">
+	  <div></div>
+	  <div></div>
+	  <div></div>
+	</div>
+</section>
+
+<section id="foot">
+	<div class="mail">
+		<p>Say Hello</p>
+		<p>info@northcorner-haven.com</p>
+		<p>803 577 9890</p>
+	</div>
+	<div class="social">
+		<div id="face"></div>
+		<div id="insta"></div>
+	</div>
+	<div class="address"></div>
+	<div class="copyright">
+		<p>Site by SDCO Parters</p>
+	</div>
+</section>
+
 <?php get_footer();
